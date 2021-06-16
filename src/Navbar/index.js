0406eead18b9582cafcf6645f166b3b900b508dc
Adam La Morre <alamorre@gmail.com>
2021-06-15 18:29:48 -0700

    ready for acct popover

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useEffect } from 'react'
+import React, { useContext, useRef, useEffect, useState } from 'react'
 
 import { sellers } from '../data'
 import { Context } from '../data/context'
@@ -6,19 +6,26 @@ import { Context } from '../data/context'
 import { getUsers } from './getUsers'
 import { createUser } from './createUser'
 
+import { Menu } from 'antd';
+import { UserOutlined, MessageOutlined } from '@ant-design/icons';
+
 const Navbar = () => {
     const didMountRef = useRef(false)
+    const [current, setCurrent] = useState(null)
     const { currentUser, setCurrentUser, setUsers } = useContext(Context)
 
     function syncUsers() {
         getUsers(users => {
             console.log('Fetched users', users)
-            sellers.map(seller => {
-                if(!users.find(user => seller.username === user.username)) {
-                    console.log('Creating user', seller.username)
-                    createUser(seller)
-                }
-            })
+            
+            {/* Run this code to load Chat Engine users */}
+            // sellers.map(seller => {
+            //     if(!users.find(user => seller.username === user.username)) {
+            //         console.log('Creating user', seller.username)
+            //         createUser(seller)
+            //     }
+            // })
+
             setUsers(users)
             setCurrentUser(sellers[0])
         })
@@ -38,21 +45,23 @@ const Navbar = () => {
 
     return (
         <div style={{ display: 'inline-block', width: '100%', backgroundColor: '#bae7ff' }}>
-            <a href='/'>
-                <h1 style={{ color: 'black', display: 'inline-block', paddingTop: '10px', paddingLeft: '12px', cursor: 'pointer' }}>
-                    Textbook Market
-                </h1>
-            </a>
-        
-            <div style={{ float: 'right', padding: '12px' }}>
-                <div>Username: {currentUser.username}</div>
-                <div>User Secret: {currentUser.secret}</div>
-                <button onClick={() => changeCurrentUser('jane@doe.co')}>Change to Jane</button>
-            </div>
-
-            <div style={{ float: 'right', display: 'inline-block', padding: '40px' }}>
-                <a href='/chats'>My Chats</a>
-            </div>        
+            <Menu theme="dark" selectedKeys={[current]} mode="horizontal" style={{ height: '64px', textAlign: 'right' }}>
+                <a href='/'>
+                    <Menu.Item key="home" style={{ height: '64px', paddingTop: '12px', float: 'left', fontSize: '22px' }}>
+                        Marketplace
+                    </Menu.Item>
+                </a>
+                
+                <Menu.Item key="mail" icon={<MessageOutlined />} style={{ height: '64px', paddingTop: '10px' }}>
+                    <a href='/chats'>
+                        My Chats
+                    </a>
+                </Menu.Item>
+                
+                <Menu.Item key="app" icon={<UserOutlined />} style={{ height: '64px', paddingTop: '10px' }}>
+                    My Account
+                </Menu.Item>
+            </Menu>
         </div>
     );
 }
